4025c7e0c1092838b39bbeca1bcf914972e47291
Simpsond87 <simpson.d87@gmail.com>
2017-08-24 14:23:31 -0400

    Built project

--- database/migrations/2017_08_24_171309_tasks.php
+++ database/migrations/2017_08_24_171309_tasks.php
@@ -13,7 +13,11 @@ class Tasks extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('tasks', function(Blueprint $table) {
+          $table->increments('id');
+          $table->string('taskContent');
+          $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +27,6 @@ class Tasks extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('tasks');
     }
 }
